b7b388163a32c8fcaf02cd7a82dee72752e028cc
piggecutlet <234113501+piggecutlet@users.noreply.github.com>
2026-03-27 18:07:47 +0900

    APK 出力パスの修正

--- src/piggecutlet/constant/PathConstant.java
+++ src/piggecutlet/constant/PathConstant.java
@@ -60,9 +60,8 @@ public class PathConstant {
   /** 「workspace\temp」ディレクトリ. */
   public static final Path TEMP_DIR = WORKSPACE_DIR.resolve("temp");
 
-  /** 「output\app.apk」. */
-  public static final Path OUTPUT_APK =
-      WORKSPACE_DIR.resolve("output").resolve(APP_APK.getFileName());
+  /** 「output\app」. */
+  public static final Path OUTPUT_APK = WORKSPACE_DIR.resolve(APP_DIR_NAME);
 
   /** 「workspace\app\lib」ディレクトリ(apktool.jar). */
   public static final Path LIB_DIR_BY_APKTOOL = APP_DIR.resolve("lib");
